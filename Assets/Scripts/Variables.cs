52aa60d840ddd018f1c073395ba24c64dff239ad
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-01 17:19:26 +0300

    commit to variables

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
+ //Type Name Value
+    int power = 15;//digit
+    double health = 100.5;//64 decimal
+    float armor = 200.5f;//32
+
+    string name = "hope";
+    bool isAlive = true;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //This game started do something
+
+        if (isAlive==false)
+        {
+            Debug.Log("Player is dead");
+        }
     }
 
     // Update is called once per frame
