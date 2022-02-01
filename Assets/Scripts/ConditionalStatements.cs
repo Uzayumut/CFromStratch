4d2a2162988fb1d0c008a24b506950e88219feb3
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-01 19:15:32 +0300

    Updated Functions Script

--- Assets/Scripts/ConditionalStatements.cs
+++ Assets/Scripts/ConditionalStatements.cs
@@ -7,6 +7,63 @@ public class ConditionalStatements : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        int a = 7;
+        int b = 5;
+        int c = 4;
+        //**************************************************IF, ELSE IF, ELSE
+
+        //< , > , <= , >= , ==     ConditionalStatements (&&-and   ||-or
+
+        if (a > b&& a > c)//(>) //AND
+        {
+            print("'A' is greater than 'B' AND 'A' is greater than 'C'");
+            if(b > c)
+            {
+                print("'B' is greater than 'C'");
+            }
+            else if(b < c)
+            {
+                print("'B' is smaller than 'C'");
+            }
+            else
+            {
+                print("'B' is equal to 'C'");
+            }
+        }
+
+        else if (a < b || a > c) //OR
+        {
+            print("'A' is greater than 'B' OR 'A' is greater than 'C'");
+        }
+        else if (a == b)
+        {
+            print("A is equal to B ");
+        }
+        else if(a<=b)//(<)
+        {
+            print("A in smaller than B");
+        }
+        
+        if (true)
+        {
+            //this block of code
+            print("This is true");
+        }
+        //*********************************SWITCH CASE********************************
+        switch (a)
+        {
+            case 7:
+                print("A is equal to 10");
+                break;
+            case 1:
+                break;
+            case 3:
+                break;
+            default:
+                print("Default value");
+                break;
+        }
+
         
     }
 
