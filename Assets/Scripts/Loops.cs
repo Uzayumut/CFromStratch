ada91a97b955d235d82213c2a9025d14c5fd91fd
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-01 20:45:59 +0300

    Updated Loops Script

--- Assets/Scripts/Loops.cs
+++ Assets/Scripts/Loops.cs
@@ -7,7 +7,32 @@ public class Loops : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //******************************for*************************************
+
+        for (int i = 0; i < 10; i++)
+            {
+                print("i= "+i);
+            }
+
+        //*******************************while***********************************
+
+        int y=0;
+
+        while ( y<=10)
+        {
+            
+            print("y= "+y);
+            y++;
+        }
+
+        //*********************************do*********************************
+
+        int z = 0;
+        do
+        {
+            print("z= "+z);
+            z++;
+        } while (z<10);
     }
 
     // Update is called once per frame
