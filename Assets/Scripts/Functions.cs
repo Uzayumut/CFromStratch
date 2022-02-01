a6aceda97ce840b0051d915b64b40a99b552783d
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-01 18:40:22 +0300

    Updated Functions

--- Assets/Scripts/Functions.cs
+++ Assets/Scripts/Functions.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,50 @@ public class Functions : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        CalculateTwoNumbers();
+        CalculateTwoNumbers(2, 3);
+        CalculateTwoNumbers(7, 4);
+
+        print(IntCalculateTwoNumbers()) ;
+        print(IntCalculateTwoNumbers(4,7));
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
         
+    }
+    //Functions
+    void Movement()//VOİD function means it does not return anything.
+    {
+        //block of code
+        
+    }
+    
+    void CalculateTwoNumbers()//METHOD WITHOUT PARAMETERS
+    {
+        int sum = 4 + 5;
+        print("The sum of a and b is " + sum);
+    }
+    void CalculateTwoNumbers(int a,int b)//METHOD WITH PARAMETERS
+    {
+        int sum = a + b;
+        print("The sum of a and b is " + sum);
+    }
+    int IntCalculateTwoNumbers()//(INT STRİNG BOOL =>RETURN)
+    {
+        int sum= 3 + 5;
+        return sum;
+    }
+    int IntCalculateTwoNumbers(int a,int b)//(INT STRİNG BOOL =>RETURN) with parameters
+    {
+        int sum = a + b;
+        return sum;
+    }
+    int CalculateTwoNumbers(int a)
+    {
+        int sum = a;
+        return sum;
     }
 }
