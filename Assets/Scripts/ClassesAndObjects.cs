1bbc5fe01d9972928b2338ca1f0a6288c1c163f4
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-02 15:20:16 +0300

    Updated ClassesAndObjects Script

--- Assets/Scripts/ClassesAndObjects.cs
+++ Assets/Scripts/ClassesAndObjects.cs
@@ -8,11 +8,42 @@ public class ClassesAndObjects : MonoBehaviour
     void Start()
     {
         
+        Player warrior = new Player();
+        warrior.PlayerInfo();
+
+        Player mage = new Player();
+        mage.role = "Mage";
+        mage.health = 150;
+        mage.PlayerInfo();
+
+        mage = warrior;
+
+        print("\n");
+
+        warrior.PlayerInfo();
+        mage.PlayerInfo();
+
+        mage.role = "Rogue";
+        mage.health = 120;
+
+        print("\n");
+
+        warrior.PlayerInfo();
+        mage.PlayerInfo();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+}
+public class Player 
+{
+    public string role = "Warrior";
+    public float health = 200;
+    public void PlayerInfo()
+    {
+        Debug.Log("The role of a Player is " + role + "and the health is " + health);
     }
 }
