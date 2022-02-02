05b28fa611d1b952f380742b1c2e63501793d51f
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-02 17:30:05 +0300

    Updated Inheritance Script

--- Assets/Scripts/Inheritance.cs
+++ Assets/Scripts/Inheritance.cs
@@ -7,7 +7,13 @@ public class Inheritance : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Warrior warrior = new Warrior();
+        warrior.WarriorInfo();
+        warrior.health = 3;
+    }
+    void Defense()
+    {
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/Warrior.cs
+++ Assets/Scripts/Warrior.cs
@@ -2,8 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Warrior : MonoBehaviour
+public class Warrior : Inheritance
 {
-    // Start is called before the first frame update
-    
+    public float health=200;
+    public string role="Warrior";
+    public Warrior()
+    {
+
+    }
+    public Warrior(string _role,float _health)
+    {
+        this.role = _role;
+        this.health = _health;
+    }
+    private void Start()
+    {
+        
+    }
+    public void WarriorInfo()
+    {
+        print("This player is role "+role+" and the health is "+health);
+    }
+    public void Attack()
+    {
+        print("The warrior attacking");
+    }
+
 }
