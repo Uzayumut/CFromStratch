e4b48b151a9f915f190ed8c897a1bf7bbf420204
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-02 14:51:42 +0300

    Updated ArraysAndFunctions Script

--- Assets/Scripts/ArraysAndFunctions.cs
+++ Assets/Scripts/ArraysAndFunctions.cs
@@ -4,16 +4,41 @@ using UnityEngine;
 
 public class ArraysAndFunctions : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        int[] numbers = new int[10];
+        ProcessArrays(numbers,0,100);
+        numbers = ProcessArrays2(numbers);
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            print("This value 2 is " + numbers[i]);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void ProcessArrays(int[] numbers, int min, int max)
+    {
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Random.Range(min, max);
+            print("This value is " + numbers[i]);
+        }
+    }
+    int[] ProcessArrays2(int[] numbers)
+    {
+        numbers = new int[10];
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Random.Range(0, 100);
+        }
+        return numbers;
     }
     
 }
