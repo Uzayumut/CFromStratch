d7e3d395e2fc9da145b0ed7c6a0720b258ad3358
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-02 16:08:22 +0300

    Added Constructor Script

--- Assets/Scripts/Constructor.cs
+++ Assets/Scripts/Constructor.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Constructor : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string name="Rage";
+    public int power=15;
+    public Constructor()
+    {
+        PowerInfo();
+    }
+    public Constructor(string _name,int _power)
+    {
+        this.name = _name;
+        this.power = _power;
+        PowerInfo();
+    }
+    void PowerInfo()
+    {
+        Debug.Log("The name of a Power is " + name + " and the power strengt is " + power);
+    }
     void Start()
     {
+        Player warrior = new Player();
         
     }
 
@@ -14,5 +30,10 @@ public class Constructor : MonoBehaviour
     void Update()
     {
         
+    }
+    public class Player2
+    {
+        Constructor rage = new Constructor("Rage",15);
+        Constructor fire = new Constructor("Fire", 10);
     }
 }
