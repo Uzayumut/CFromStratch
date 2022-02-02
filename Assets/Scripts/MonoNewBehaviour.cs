e0175eeb3ab6220a3d0f840aa1e3d06cb02c07eb
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-02 17:38:56 +0300

    Added MonoNewBehaviour Script

---
+++ Assets/Scripts/MonoNewBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoNewBehaviour : MonoBehaviour
+{
+    [SerializeField]//The Inspector screen visible
+    private string role;
+    [HideInInspector]//The ınspector screen not visible
+    public int health;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
