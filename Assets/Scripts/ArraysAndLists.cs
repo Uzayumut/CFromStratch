76856715ac671cfed42a23be963024abf72b1745
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-02 18:50:53 +0300

    Updated ArraysAndLists Script

--- Assets/Scripts/ArraysAndLists.cs
+++ Assets/Scripts/ArraysAndLists.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class ArraysAndLists : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private int[] numbers = new int[10];
+    public List<int> numberList = new List<int>(10);
     void Start()
     {
-        
+        //for (int i = 0; i < numberList.Count; i++)
+        //{
+        //    numberList[i] = 3;
+        //}
+        //numberList.Add(19);//Added list
+        //numberList.RemoveAt(5);//remove list
+        //numberList.Reverse();
     }
 
     // Update is called once per frame
