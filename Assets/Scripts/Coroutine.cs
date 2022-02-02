a63f699d0e48620a20118e65141875abda3f7613
Uzay Bardakci <uzaybardakci.cme@gmail.com>
2022-02-02 18:23:44 +0300

    Updated Coroutine Script

--- Assets/Scripts/Coroutine.cs
+++ Assets/Scripts/Coroutine.cs
@@ -8,11 +8,25 @@ public class Coroutine : MonoBehaviour
     void Start()
     {
         
+
+        StartCoroutine(PrintAfterDelay(2));//start coroutine code.
+        StartCoroutine("PrintAfterDelay");//same
+        StopCoroutine("PrintAfterDelay");//stop coroutine code
+        StopAllCoroutines();//stop all coroutine code
+        //Time.timeScale = 0;
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    IEnumerator PrintAfterDelay(float time)//How much does it need to wait
+    {
+        yield return new WaitForSeconds(time);
+        //yield return new WaitForSecondsRealtime(2);
+        print("something");
     }
 }
